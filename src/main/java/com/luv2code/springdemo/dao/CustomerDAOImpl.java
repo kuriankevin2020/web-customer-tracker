3bd4dd8e01f49cdba972b123da8f7ed28dfea542
Kurian Kevin <kuriankevin2020@gmail.com>
2021-10-24 17:15:19 +0530

    Initial Commit Web_Customer_Tracker

---
+++ src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,50 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public List<Customer> getCustomers() {
+		Session session = sessionFactory.getCurrentSession();
+		Query<Customer> theQuery = session.createQuery("from Customer order by lastName", Customer.class);
+		List<Customer> customers = theQuery.getResultList();
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer customer) {
+		Session session = sessionFactory.getCurrentSession();
+		session.saveOrUpdate(customer);
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+		Session session = sessionFactory.getCurrentSession();
+		Customer customer = session.get(Customer.class, theId);
+		return customer;
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+		Session session = sessionFactory.getCurrentSession();
+		Query theQuery = session.createQuery("delete from Customer where id=:customerId");
+		theQuery.setParameter("customerId", theId);
+		theQuery.executeUpdate();
+//		Customer customer = session.get(Customer.class, theId);
+//		session.delete(customer);
+	}
+
+}
